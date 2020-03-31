e6ee37a2597dc9be7d681a91295ec56987d93ce2
Jacques Kang <jkang.perso@gmail.com>
2020-03-31 16:54:01 +0200

    feat: implement get position function

--- Covid19Reporter/Covid19Functions/GetPositionFunction.cs
+++ Covid19Reporter/Covid19Functions/GetPositionFunction.cs
@@ -1,39 +1,25 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Covid19Functions
 {
     public static class GetPositionFunction
     {
         [FunctionName("get-position")]
-        public static async Task<IActionResult> Run(
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             [CosmosDB(databaseName: "cosmosdb-covid19", collectionName: "positions",
                 ConnectionStringSetting = "CosmosDBConnectionString",
-                SqlQuery = "select VALUE * from ToDoItems r where r.id = {id}")]
-                IEnumerable<ToDoItem> toDoItems
+                SqlQuery = "select c.userName, c.dateTime, c.position, c.isSuspected from c")]
+            IEnumerable<dynamic> positions,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            log.LogInformation($"C# HTTP trigger function processed a request: {req.Path}");
+            return new OkObjectResult(positions);
         }
     }
 }
