381937b3e508b88b736abeb9b0d96edccb2ac028
Jacques Kang <jkang.perso@gmail.com>
2020-03-31 15:01:55 +0200

    wip

---
+++ Covid19Reporter/Covid19Functions/GetPositionFunction.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Covid19Functions
+{
+    public static class GetPositionFunction
+    {
+        [FunctionName("get-position")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName: "cosmosdb-covid19", collectionName: "positions",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                SqlQuery = "select VALUE * from ToDoItems r where r.id = {id}")]
+                IEnumerable<ToDoItem> toDoItems
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
